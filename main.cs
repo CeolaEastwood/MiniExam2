75c945c1ca11029e879c6362a743d5de6bc3969b
CeolaEastwood <CeolaEastwood@gmail.com>
2021-03-11 18:24:06 +0000

    debug

--- main.cs
+++ main.cs
@@ -4,30 +4,32 @@ class MainClass {
 
 public static double CylinderVolume(double rad, double htlog)
   {  
-    int volume =  3.14*rad*rad*htlog ;
+    double CylinderVolume = 3.14*rad*rad*htlog;
     
     return CylinderVolume;
    
   }
 
-public static void Main (string[] args);
+public static void Main (string[] args){
 
   Console.WriteLine("How many volumes to calculate?");
   double n = Convert.ToDouble(Console.ReadLine());
 
-  for (i=0; i<n;i++)
-  {
-  Console.WriteLine("What is the radius?");
-  double r = Convert.ToDouble(Console.ReadLine());
+  for (int i=0; i<n;i++)
+   {
+    Console.WriteLine("What is the radius?");
+    double r = Convert.ToDouble(Console.ReadLine());
 
-  Console.WriteLine("What is the height?");
-  double h = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("What is the height?");
+    double h = Convert.ToDouble(Console.ReadLine());
 
-  cvolume = CylinderVolume(r, h);
+    double cvolume = CylinderVolume(r, h);
 
-  Console.WriteLine("The volume of the cylinder is" + cvolume );
-  }
+    Console.WriteLine("The volume of the cylinder is " + cvolume);
+    }
   
-   
-  }
+
+ }
+  
+  
 }
